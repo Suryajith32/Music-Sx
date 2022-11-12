02e566d7923ff6f9d55d7736e1f4950090f4ed39
ini esta <suryajithms32@gmail.com>
2022-11-12 12:27:32 +0530

    re Commited Musix-Sx

---
+++ client/src/AdminComponents/AdminNavbar.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import { useNavigate } from 'react-router-dom';
+
+function AdminNavbar() {
+  const navigate = useNavigate()
+
+const LogoutHandler=()=>{
+  localStorage.removeItem("admin");
+  navigate('/adminlogin')
+}
+
+  return (
+    
+        <Box  sx={{ flexGrow: 1, position: "absolute" }}>
+      <AppBar sx={{ bgcolor: "rgba(0, 0, 0,0.31)", background: "linear-gradient(to right, #0f0c29, #302b63, #24243e)",  }}  >
+        <Box position="sticky">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Admin 
+          </Typography>
+          <Button onClick={LogoutHandler} color="inherit">Logout</Button>
+        </Toolbar>
+        </Box>
+      </AppBar>
+    </Box>
+   
+  )
+}
+
+export default AdminNavbar
---
+++ client/src/AdminPages/AdminHome/AdminHome.js
@@ -0,0 +1,19 @@
+import { Box, Stack } from '@mui/material'
+import React from 'react'
+import AdminFeed from '../../AdminComponents/AdminFeed'
+import AdminNavbar from '../../AdminComponents/AdminNavbar'
+import Sidebar from '../../AdminComponents/Sidebar'
+
+function AdminHome() {
+  return (
+    <Box>
+       <Stack direction="row" spacing={1} justifyContent="space-between">
+        <Sidebar/>
+        <AdminFeed/>
+        </Stack> 
+        <AdminNavbar/>
+    </Box>
+  )
+}
+
+export default AdminHome
